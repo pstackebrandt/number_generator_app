e3336ca8ffaa30d80cdcf4fa35062981c316e1f4
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2023-10-08 15:32:35 +0200

    Set used php version to 8.1 Add test suite and default test

---
+++ tests/unit/DefaultTest.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+class DefaultTest extends TestCase
+{
+    public function testTrueIsTrue(): void
+    {
+        $this->assertTrue(true);
+    }
+}
